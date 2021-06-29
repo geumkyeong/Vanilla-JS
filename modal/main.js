ca11459f7a197e6d89e9f69333c341f915ff3b97
geumkyeong <ansrmarud@gmail.com>
2021-06-29 13:56:41 +0900

    modal close

--- modal/main.js
+++ modal/main.js
@@ -1,8 +1,16 @@
 const openBtn = document.querySelector('#open');
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.modal__overlay');
+const closeBtn = document.querySelector('#close');
 
 const openModal = () => {
     modal.classList.remove('hidden');
 }
 
+const closeModal = () => {
+    modal.classList.add('hidden');
+}
+
 openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
