69654f53cdd532c686c8a9beef5885bab0307179
geumkyeong <ansrmarud@gmail.com>
2021-07-03 13:53:03 +0900

    todo-list Save and Load in localStorage

--- todo-list/main.js
+++ todo-list/main.js
@@ -6,18 +6,25 @@ const TODOS_LIST = 'toDos';
 
 const toDos = [];
 
-function paintTodo(text) {
+// localStorage에 toDos 배열을 저장
+function saveTodos() {
+    // object -> string
+    localStorage.setItem(TODOS_LIST, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    // 화면에 Element 생성
     const li = document.createElement('li');
     const deleteBtn = document.createElement('button');
     const span = document.createElement('span');
     const newId = toDos.length + 1;
 
-    li.id = newId; // id
     deleteBtn.innerText = '❌'; 
     span.innerText = text; 
+    li.id = newId; // id
 
     li.appendChild(deleteBtn); // 삭제하는 버튼
-    li.appendChild(span); // todo 내용
+    li.appendChild(span); // todo
     toDoList.appendChild(li);
 
     
@@ -25,23 +32,31 @@ function paintTodo(text) {
         text: text, // 입력창에 작성한 내용
         id: newId // id
     };
-
+    // toDos: 객체 배열
     toDos.push(toDoObj);
+    // localStorage에 저장
+    saveTodos();
 }
 
 function handleSubmit(event) {
     event.preventDefault();
     // 입력창에 작성한 내용
     const currentValue = toDoInput.value;
-    paintTodo(currentValue);
+    paintToDo(currentValue);
     toDoInput.value = '';
 }
 
-// todo list 화면에 띄우기
+// localStorage에 저장된 toDos 배열을 화면에 띄우기
 function loadToDos() {
     const loadedToDos = localStorage.getItem(TODOS_LIST);
     if(loadedToDos !== null) {
-
+        // string -> object
+        const parsedToDos = JSON.parse(loadedToDos);
+        // forEach: array, string, object의 요소 하나하나에 method를 적용시킴
+        parsedToDos.forEach(function(toDo){
+            // object의 text를 가져옴
+            paintToDo(toDo.text);
+        });
     }
 }
 
