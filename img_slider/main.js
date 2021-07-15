fea216efbea5eba26a2a74ff50a590b4f886cb94
geumkyeong <ansrmarud@gmail.com>
2021-07-15 16:06:34 +0900

    IMG SLIDER - Active Item

--- img_slider/main.js
+++ img_slider/main.js
@@ -6,44 +6,47 @@ const prevBtn = document.querySelector('#prevBtn');
 
 let index = 0;
 let lastIndex = slides.length-1; // 2
-const slideWidth = slides[0].clientWidth; // *Slider Size(579px)
+const slideWidth = slides[0].clientWidth; // *Slider 크기(579px)
 
 nextBtn.addEventListener('click', () => {
     items.forEach((item) => { item.classList.remove('active'); }); // 활성화된 Active Style 초기화
-
     slidesContainer.style.transition = "all 0.3s ease-in-out"; // 슬라이딩 애니메이션
-
+    // 다음 위치로
     index++;
     slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
 
-    // 마지막 Slide 일 때,
+    // 마지막 슬라이드에서 넘어갈 때,
     if(index > lastIndex){
         setTimeout(() => {
-            index = 0;
-            slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+            // 첫 슬라이드 위치로 가기 
+            index = 0; 
+            slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`; // 0px
             items[index].classList.add('active');
-        }, 200); // 다음 위치로 이동하다가 첫 Slide로 가기 
+            // console.log('Current Direction: ', slidesContainer.style.transform);
+        }, 200);
     } else {
-        items[index].classList.add('active'); // 현재 item Active 하기
+        items[index].classList.add('active'); // 현재 아이템 Active 하기
+        // console.log('Current Direction: ', slidesContainer.style.transform);
     }
-    console.log('Current Direction: ', slidesContainer.style.transform);
 });
 
 prevBtn.addEventListener('click', () => {
     items.forEach((item) => { item.classList.remove('active'); }); // 활성화된 Active Style 초기화
     slidesContainer.style.transition = "all 0.3s ease-in-out"; // 슬라이딩 애니메이션
-
+    // 이전 위치로
     index--;
     slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
-    // 첫 Slide 일 때,
+    // 첫 슬라이드에서 넘어갈 때,
     if(index < 0){
         setTimeout(() => {
-            index = lastIndex;
-            slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+            // 마지막 슬라이드 위치로 가기
+            index = lastIndex; 
+            slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`; // -1158px
             items[index].classList.add('active');
-        }, 200); // 이전 위치로 이동하다가 마지막 Slide로 가기 
+            // console.log('Current Direction: ', slidesContainer.style.transform);
+        }, 200); 
     } else {
-        items[index].classList.add('active'); // 현재 item Active 하기
+        items[index].classList.add('active'); // 현재 아이템 Active 하기
+        // console.log('Current Direction: ', slidesContainer.style.transform);
     }
-    console.log('Current Direction: ', slidesContainer.style.transform);
 });
